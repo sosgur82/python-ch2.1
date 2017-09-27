4e9f6fa56905487f248ef7fe08d9102bfea12aeb
sosgur82 <sosgur82@gamil.com>
2017-09-27 15:15:49 +0900

    asdasd

---
+++ variable.py
@@ -0,0 +1,22 @@
+#변수 이름은 문자, 숫자, _ 로 구성해야 한다.
+
+import keyword
+
+friend = 1
+a= 10
+my_name = '김계현'
+_yourName = '둘리'
+member1 = '도우넛'
+
+
+#에러
+#$friend = 2
+#a! = 1
+#1abc = 10
+
+
+print(keyword.kwlist)
+
+#한글이름 변수 가능하다
+요금1 = 2000
+print(요금1)
